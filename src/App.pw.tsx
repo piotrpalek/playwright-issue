5e4c934602b44af3342b4eb80b015cd18487c8dd
Piotr P <piotr.palek@housecallpro.com>
2022-10-05 08:32:11 +0200

    init

---
+++ src/App.pw.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { test, expect } from '@playwright/experimental-ct-react';
+import 'spec-utils/apiMock.ts';
+import App from './App';
+
+test.use({ viewport: { width: 500, height: 500 } });
+
+test('should work', async ({ mount }) => {
+  const component = await mount(<App></App>);
+  await expect(component).toContainText('Hello world!2!');
+});
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Main = () => {
+  return <h1>Hello world!2!</h1>;
+};
+
+export default Main;
