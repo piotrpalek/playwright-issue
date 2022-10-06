00edac4e560635b2b6e6cda791c6c9b6354633b0
Piotr P <piotr.palek@housecallpro.com>
2022-10-06 13:13:13 +0200

    nested non-js bug

--- spec/javascripts/utils/apiMock.ts
+++ spec/javascripts/utils/apiMock.ts
@@ -1,6 +1,8 @@
+import nonTsUtil from 'spec-utils/nonTsUtil';
 import otherUtil from './otherUtil';
 
 export default () => {
   otherUtil();
+  nonTsUtil();
   console.log('mocking API');
 }
---
+++ spec/javascripts/utils/nonTsUtil.js
@@ -0,0 +1,6 @@
+import nestedNonTsUtil from 'spec-utils/nestedNonTsUtil';
+
+export default () => {
+  nestedNonTsUtil();
+  console.log('nonTsUtil')
+};
