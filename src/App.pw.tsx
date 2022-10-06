b3ddad983d5141912500c28d51b8f708a0c0d00f
Piotr P <piotr.palek@housecallpro.com>
2022-10-06 09:06:09 +0200

    commonjs

--- src/App.pw.tsx
+++ src/App.pw.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { test, expect } from '@playwright/experimental-ct-react';
 import 'spec-utils/apiMock.ts';
+import { noop } from 'lodash-es';
 import App from './App';
 
 test.use({ viewport: { width: 500, height: 500 } });
 
 test('should work', async ({ mount }) => {
+  noop();
   const component = await mount(<App></App>);
   await expect(component).toContainText('Hello world!2!');
 });
